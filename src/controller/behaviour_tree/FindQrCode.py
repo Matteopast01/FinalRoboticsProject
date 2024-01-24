722558627f9f83da82f4c7b5f0d13683c0207db2
Roberto Di Stefano <robertodistefano24@gmail.com>
2024-01-24 20:01:13 +0100

    done FindQrCode.py

--- src/controller/behaviour_tree/FindQrCode.py
+++ src/controller/behaviour_tree/FindQrCode.py
@@ -4,22 +4,31 @@ from py_trees.common import Status
 from py_trees.composites import Sequence
 from py_trees.composites import Selector
 from py_trees import logging as log_tree
+from isrlab_project.controller.main import Controller
+from isrlab_project.controller.Knowledge import Knowledge
 
 
 class FinalQrCode(Behaviour):
-  def __init__(self, name):
-    super(FinalQrCode, self).__init__(name)
+    _controller: Controller
 
+    def __init__(self, name, controller):
+        super(FinalQrCode, self).__init__(name)
+        self._controller = controller
 
-  def setup(self):
-    self.logger.debug(f"FinalQrCode::setup {self.name}")
+    def setup(self):
+        self.logger.debug(f"FinalQrCode::setup {self.name}")
 
-  def initialise(self):
-    self.logger.debug(f"FinalQrCode::initialise {self.name}")
+    def initialise(self):
+        self.logger.debug(f"FinalQrCode::initialise {self.name}")
 
-  def update(self):
-    self.logger.debug(f"FinalQrCode::update {self.name}")
+    def update(self):
+        self.logger.debug(f"FinalQrCode::update {self.name}")
+        self._controller.perform_action("stop")
+        if Knowledge().get_arrived_data()["arrived"]:
+          return Status.SUCCESS
+        else:
+          self._controller.perform_action("turn_left")
+          return Status.RUNNING
 
-  def terminate(self, new_status):
-    self.logger.debug(f"FinalQrCode::terminate {self.name} to {new_status}")
-
+    def terminate(self, new_status):
+        self.logger.debug(f"FinalQrCode::terminate {self.name} to {new_status}")
