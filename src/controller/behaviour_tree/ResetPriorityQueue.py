0097697b018f7122b324d45f534cc0e086796b60
Roberto Di Stefano <robertodistefano24@gmail.com>
2024-01-25 10:14:09 +0100

    Done ResetPriorityQueue.py

--- src/controller/behaviour_tree/ResetPriorityQueue.py
+++ src/controller/behaviour_tree/ResetPriorityQueue.py
@@ -5,6 +5,7 @@ from py_trees.composites import Sequence
 from py_trees.composites import Selector
 from py_trees import logging as log_tree
 from isrlab_project.controller.main import Controller
+from isrlab_project.controller.Knowledge import Knowledge
 
 
 class ResetPriorityQueue(Behaviour):
@@ -22,6 +23,9 @@ class ResetPriorityQueue(Behaviour):
 
     def update(self):
         self.logger.debug(f"ResetPriorityQueue::update {self.name}")
+        goal = Knowledge().get_goal()
+        Knowledge().get_graph().reset_priority_queue(goal)
+        return Status.SUCCESS
 
     def terminate(self, new_status):
         self.logger.debug(f"ResetPriorityQueue::terminate {self.name} to {new_status}")
